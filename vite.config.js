c5c2f66a5f183a65a5a0cf1cd3f7227e09a6e071
massongit <suzukimasaya428@gmail.com>
2023-11-30 08:08:22 +0900

    pdfjs-distのworkerやcmapsをパッケージから取得する

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./node_modules/pdfjs-dist/cmaps/",
+          dest: "assets"
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
