cef93816f02b8090049c757187f8171756550db0
Restyled.io <commits@restyled.io>
2023-11-29 23:34:32 +0000

    Restyled by prettier

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import vue from '@vitejs/plugin-vue'
-import {fileURLToPath, URL} from 'node:url'
-import {defineConfig} from 'vite'
-import {viteStaticCopy} from 'vite-plugin-static-copy'
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins : [
-    vue(), viteStaticCopy({
-      targets : [ {src : "./node_modules/pdfjs-dist/cmaps/", dest : "assets"} ]
-    })
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [{ src: "./node_modules/pdfjs-dist/cmaps/", dest: "assets" }],
+    }),
   ],
-  resolve : {alias : {'@' : fileURLToPath(new URL('./src', import.meta.url))}}
-})
+  resolve: { alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) } },
+});
